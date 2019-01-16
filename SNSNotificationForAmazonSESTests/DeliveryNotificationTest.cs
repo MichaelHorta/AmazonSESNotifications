34b87d6d00b2fdff7b66033cbd5bd08d75c29090
Michael Horta Fleitas <michaelhorta32@gmail.com>
2019-01-16 16:53:34 -0500

    Class DeliveryNotificationTest

---
+++ SNSNotificationForAmazonSESTests/DeliveryNotificationTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using SNSNotificationForAmazonSES.Models;
+
+namespace SNSNotificationForAmazonSESTests
+{
+    [TestFixture]
+    public class DeliveryNotificationTest
+    {
+        [Test(Description = "Should serialize a delivery notification (formatted as JSON) in an object")]
+        public void SerializeDeliveryNotification()
+        {
+            var resource = ResourceManager.RetrieveEmbeddedResource("delivery-notification-content");
+            var deliveryNotification = Newtonsoft.Json.JsonConvert.DeserializeObject<AmazonSESDeliveryNotification>(resource);
+            Assert.IsTrue(null != deliveryNotification);
+        }
+    }
+}
