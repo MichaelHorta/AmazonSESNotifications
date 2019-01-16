d50dc940c4d174a4fcd59a766a7b4e6af0566462
Michael Horta Fleitas <michaelhorta32@gmail.com>
2019-01-16 16:54:40 -0500

    Class ResourceManager and resource delivery-notification-content.json

---
+++ SNSNotificationForAmazonSESTests/ResourceManager.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace SNSNotificationForAmazonSESTests
+{
+    public class ResourceManager
+    {
+
+        /// <summary>
+        /// Retrieve the embedded resource as string.
+        /// </summary>
+        /// <param name="resourceName"></param>
+        /// <returns></returns>
+        public static string RetrieveEmbeddedResource(string resourceName)
+        {
+            using (Stream stream = RetrieveEmbeddedResourceAsStream(resourceName))
+            {
+                StreamReader reader = new StreamReader(stream);
+                return reader.ReadToEnd();
+            }
+        }
+
+        /// <summary>
+        /// Retrieve the embedded resource as byte array.
+        /// </summary>
+        /// <param name="resourceName"></param>
+        /// <returns></returns>
+        public static byte[] RetrieveEmbeddedResourceAsByteArray(string resourceName)
+        {
+            using (Stream resFileStream = RetrieveEmbeddedResourceAsStream(resourceName))
+            {
+                if (resFileStream == null) return null;
+                byte[] bytes = new byte[resFileStream.Length];
+                resFileStream.Read(bytes, 0, bytes.Length);
+                return bytes;
+            }
+        }
+
+        /// <summary>
+        /// Retrieve the embedded resource as stream.
+        /// </summary>
+        /// <param name="resourceName"></param>
+        /// <returns></returns>
+        public static Stream RetrieveEmbeddedResourceAsStream(string resourceName)
+        {
+            return Assembly.GetExecutingAssembly().GetManifestResourceStream(string.Format("{0}.{1}", "SNSNotificationForAmazonSESTests.Resources", "delivery-notification-content.json"));
+        }
+    }
+}
