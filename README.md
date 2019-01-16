e37c467041fd03270a97a1f90ea08c5b0d7bf7f9
Michael Horta Fleitas <michaelhorta32@gmail.com>
2019-01-16 13:15:53 -0500

    Project SNSNotificationForAmazonSESTests for the unit tests

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# SNSNotificationForAmazonSES
+SNS Notifications For Amazon SES (.NET)
+===============================
+
+
+Unit tests
+-----
+After build the project **SNSNotificationForAmazonSESTests**, run unit tests with the tool **console runner** installed by the package [NUnit.ConsoleRunner][NUnit.ConsoleRunner]
+
+`$ packages\NUnit.ConsoleRunner.3.9.0\tools\nunit3-console.exe SNSNotificationForAmazonSESTests\bin\Debug\SNSNotificationForAmazonSESTests.dll`
---
+++ SNSNotificationForAmazonSESTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SNSNotificationForAmazonSESTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SNSNotificationForAmazonSESTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bdfbc8b3-95dc-4ffb-a031-60df03ae81f8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
