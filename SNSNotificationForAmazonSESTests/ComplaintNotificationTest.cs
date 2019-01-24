8b2012e2563dab9de7884130efee4ce606a965d5
Michael Horta Fleitas <michaelhorta32@gmail.com>
2019-01-24 10:38:46 -0500

    Unit test for complaint notification

---
+++ SNSNotificationForAmazonSESTests/ComplaintNotificationTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+using SNSNotificationForAmazonSES.Models;
+
+namespace SNSNotificationForAmazonSESTests
+{
+    public class ComplaintNotificationTest
+    {
+        [Test(Description = "Should serialize a complaint notification (formatted as JSON) in an object")]
+        public void SerializeBounceNotification()
+        {
+            AmazonSESComplaintNotification complaintNotification = null;
+            var resource = ResourceManager.RetrieveEmbeddedResource("complaint-notification-content.json");
+            if (null != resource)
+            {
+                complaintNotification = Newtonsoft.Json.JsonConvert.DeserializeObject<AmazonSESComplaintNotification>(resource);
+            }
+
+            Assert.IsTrue(null != complaintNotification);
+        }
+    }
+}
