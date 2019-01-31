ad2a28f21d122496a47b3377aca898db179e74f5
Michael Horta Fleitas <michaelhorta32@gmail.com>
2019-01-31 11:53:35 -0500

    Constructors in class AmazonSESDeliveryNotification. Using methods ParseTool.ParseAmazonSESNotification and ParseTool.TryParseAmazonSESNotification

--- AmazonSESNotifications/Models/AmazonSESDeliveryNotification.cs
+++ AmazonSESNotifications/Models/AmazonSESDeliveryNotification.cs
@@ -7,5 +7,32 @@ namespace AmazonSESNotifications.Models
     {
         [JsonProperty("delivery")]
         public DeliveryObject Delivery { get; set; }
+
+        public AmazonSESDeliveryNotification()
+        {
+
+        }
+
+        public AmazonSESDeliveryNotification(string notification)
+        {
+            AmazonSESNotification amazonSESNotification = null;
+            if (TryParse(notification, out amazonSESNotification))
+            {
+                var amazonSESDeliveryNotification = amazonSESNotification as AmazonSESDeliveryNotification;
+                Mail = amazonSESDeliveryNotification.Mail;
+                NotificationType = amazonSESDeliveryNotification.NotificationType;
+                Delivery = amazonSESDeliveryNotification.Delivery;
+            }
+        }
+
+        public new static AmazonSESDeliveryNotification Parse(string notification)
+        {
+            return (AmazonSESDeliveryNotification) ParseTool.ParseAmazonSESNotification(notification, typeof(AmazonSESDeliveryNotification));
+        }
+
+        public new static bool TryParse(string notification, out AmazonSESNotification amazonSESNotification)
+        {
+            return ParseTool.TryParseAmazonSESNotification(notification, out amazonSESNotification, typeof(AmazonSESDeliveryNotification));
+        }
     }
 }
